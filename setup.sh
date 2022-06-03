3234d5a2c279610ca1fa43f13cd0be332357aa98
Ubuntu <ubuntu@ip-172-31-39-199.us-west-2.compute.internal>
2022-06-03 05:58:24 +0000

    experiment updates

--- setup.sh
+++ setup.sh
@@ -1,6 +1,3 @@
-conda env create -f environment.yml
-conda activate fasterClassifier
-python -m ipykernel install --user --name fasterClassifier
-cd approx/src/pytorch/cpp
+cd src/approx/src/pytorch/cpp
 python setup.py install
-cd ../../../../
+cd ../../../../../
